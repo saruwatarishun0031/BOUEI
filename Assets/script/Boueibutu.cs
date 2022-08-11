cced102ad652047617ad5ee7b492441526c6d4d9
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2022-08-12 03:02:18 +0900

    sisutemu

---
+++ Assets/script/Boueibutu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Boueibutu : MonoBehaviour, IDamage
+{
+    [SerializeField][Tooltip("耐久値")]
+    Slider _tp;
+    [SerializeField] int _maxtp;
+    [SerializeField] private float Currenttp;
+
+    public void ReceiveDamage(int damage)
+    {
+        Currenttp -= damage;
+        Currenttp = Currenttp - damage;
+        _tp.value = (float)Currenttp / (float)_maxtp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
